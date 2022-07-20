e3ec7889ecbdca93d7caeee36ee29489ad6c6be3
Alessandro Morales <alessandromorales0822@gmail.com>
2022-07-19 22:13:11 -0600

    comunicados para empleados

---
+++ ProFind/Lib/Admin/Views/Comunicados/Page-Comunicados.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ProFind.Lib.Admin.Views.Comunicados
+{
+    /// <summary>
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class Page_Comunicados : Page
+    {
+        public Page_Comunicados()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Menu_Opening(object sender, object e)
+        {
+            CommandBarFlyout myFlyout = sender as CommandBarFlyout;
+            if (myFlyout.Target == REBCustom)
+            {
+                AppBarButton myButton = new AppBarButton();
+                myButton.Command = new StandardUICommand(StandardUICommandKind.Share);
+                myFlyout.PrimaryCommands.Add(myButton);
+            }
+        }
+
+        private void REBCustom_Loaded(object sender, RoutedEventArgs e)
+        {
+            REBCustom.SelectionFlyout.Opening += Menu_Opening;
+            REBCustom.ContextFlyout.Opening += Menu_Opening;
+        }
+
+        private void REBCustom_Unloaded(object sender, RoutedEventArgs e)
+        {
+            REBCustom.SelectionFlyout.Opening -= Menu_Opening;
+            REBCustom.ContextFlyout.Opening -= Menu_Opening;
+        }
+    }
+}
