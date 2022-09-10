7fe35f70e957610d0a7170b52325efb0d9f5b3e4
Susan Castillo <susancasoli@gmail.com>
2022-09-09 20:44:53 -0600

    Security Questions desing complete and Answer clients too

--- ProFind/Lib/AdminNS/Views/CRUDPages/SecurityPageNS/CreatePage/CreatePage.xaml.cs
+++ ProFind/Lib/AdminNS/Views/CRUDPages/SecurityPageNS/CreatePage/CreatePage.xaml.cs
@@ -15,14 +15,14 @@ using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace ProFind.Lib.AdminNS.Views.CRUDPages.SecurityPageNS
+namespace ProFind.Lib.AdminNS.Views.CRUDPages.SecurityPageNS.CreatePage
 {
     /// <summary>
     /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
     /// </summary>
-    public sealed partial class SecurityPage : Page
+    public sealed partial class CreatePage : Page
     {
-        public SecurityPage()
+        public CreatePage()
         {
             this.InitializeComponent();
         }
