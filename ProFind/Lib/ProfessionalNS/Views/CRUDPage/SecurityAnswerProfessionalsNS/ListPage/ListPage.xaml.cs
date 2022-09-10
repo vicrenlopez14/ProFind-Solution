da3efaf04a13dc69676044e0195eba173d15b087
Susan Castillo <susancasoli@gmail.com>
2022-09-10 16:17:12 -0600

    Security answer professionals complete

--- ProFind/Lib/ProfessionalNS/Views/CRUDPage/SecurityAnswerProfessionalsNS/ListPage/ListPage.xaml.cs
+++ ProFind/Lib/ProfessionalNS/Views/CRUDPage/SecurityAnswerProfessionalsNS/ListPage/ListPage.xaml.cs
@@ -15,14 +15,14 @@ using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace ProFind.Lib.AdminNS.Views.CRUDPages.SecurityAnswerClientsNS
+namespace ProFind.Lib.ProfessionalNS.Views.CRUDPage.SecurityAnswerProfessionalsNS.ListPage
 {
     /// <summary>
     /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
     /// </summary>
-    public sealed partial class ReadPage : Page
+    public sealed partial class ListPage : Page
     {
-        public ReadPage()
+        public ListPage()
         {
             this.InitializeComponent();
         }
