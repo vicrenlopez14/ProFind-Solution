9a42b9ef6eae0fafdbb827cd366c9e8981c9bd3c
Víctor López <vicren.lopez@outlook.com>
2022-10-05 14:20:48 -0600

    Project converter

---
+++ ProFind/Lib/Global/Converters/TagDurationReadable.cs
@@ -0,0 +1,38 @@
+﻿using ProFind.Lib.Global.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace ProFind.Lib.Global.Converters
+{
+    public class TagDurationReadable : IValueConverter
+    {
+        // its a little bit tricky to inject this using IoC, but it's possible
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (!(value is int)) return "Unknown";
+
+            switch (value)
+            {
+                case 0:
+                    return "Short";
+                case 1:
+                    return "Medium";
+                case 2:
+                    return "Long";
+                default:
+                    return "Unknown";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return 0;
+        }
+    }
+}
